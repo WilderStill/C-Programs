f5cf95ee9084bcd477d3300b288957aab0fbe1d8
WilderStill <Wilderness_114514@outlook.com>
2024-08-28 21:18:33 +0800

    Add files via upload

---
+++ Sphere Online Judge(SPOJ)/SP3928 - MDIGITS - Counting Digits.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll bit[144],f[144][20];
+ll dfs(ll n,bool is_max,bool lead,ll num,ll sum)
+{
+    if(!n)return sum;
+    if(!is_max&&lead&&~f[n][sum])return f[n][sum];
+    ll m=is_max?bit[n]:9;
+    ll ans=0;
+    for(ll i=0;i<=m;i++)
+        ans+=dfs(n-1,is_max&&i==m,lead||i,num,sum+((i==num)&&(i||lead)));
+    if(!is_max&&lead)f[n][sum]=ans;
+    return ans;
+}
+ll dp_(ll x,ll num)
+{
+    ll len=0;
+    while(x)
+    {
+        bit[++len]=x%10;
+        x/=10;
+    }
+    return dfs(len,1,0,num,0);
+}
+int main()
+{
+    ll m,n;
+	memset(f,-1,sizeof(f));
+	while(true)
+	{
+		scanf("%lld%lld",&n,&m);
+		if(n>m)swap(n,m);
+		if(!n&&!m)return 0;
+    	for(ll i=0;i<=9;++i)
+		{
+			if(i)putchar(' ');
+			printf("%lld",dp_(m,i)-dp_(n-1,i));
+		}
+    	puts("");
+	}
+}
